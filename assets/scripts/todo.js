130795e6c99a4a54eb8c79cdec95a680d4c39660
Aida Hashemi <aidahashemiam@gmail.com>
2023-12-08 07:50:13 -0800

    todo almost done!just it needs to be more functional

--- assets/scripts/todo.js
+++ assets/scripts/todo.js
@@ -1,19 +1,23 @@
 const form = document.getElementById("form");
 const inputBox = document.getElementById("input-box");
 const listcontainer = document.getElementById("list");
+// const li = document.createElement("li");
+// li.setAttribute("class", "todo__item");
+// listcontainer.appendChild(li);
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  if (inputBox.value === "") {
+  if (inputBox.value.trim().length === 0) {
     alert("you must write something");
-  } else {
-    listcontainer.innerHTML += `<li class="todo__item">
-   <span class="todo__task">
+  } else if (inputBox.value.trim() !== "") {
+    listcontainer.innerHTML += `<li class="todo__item d-flex justify-between align-center">
+   <span class="todo__task d-flex gap-md align-center">
    <i class="fa-regular fa-square-check todo__check"></i>
-  <i class="far fa-square check-icon todo__square"></i>${inputBox.value}
+  <i class="far fa-square todo__square"></i><span class="todo__inputValue">${inputBox.value.trim()}</span>
    </span>
-     <span class="list-icons">
+     <span class="todo__icons d-flex gap-lg">
        <i class="fas fa-pen"></i>
-       <i class="far fa-trash-alt trash-icon"></i>
+       <i class="far fa-trash-alt todo__trash"></i>
      </span>
      </li>`;
   }
@@ -22,20 +26,26 @@ form.addEventListener("submit", (e) => {
 });
 
 listcontainer.addEventListener("click", function (e) {
-  if (e.target.classList.contains("trash-icon")) {
+  if (e.target.classList.contains("todo__trash")) {
     e.target.parentElement.parentElement.remove();
     saveData();
-  } else if (e.target.classList.contains("check-icon")) {
-    e.target.parentElement.style.textDecoration = "line-through";
+  } else if (e.target.classList.contains("todo__square")) {
     e.target.style.display = "none";
-    e.target.parentElement.querySelector(".cheked-icon").style.display =
-      "inline-block";
+    e.target.parentElement.querySelector(".todo__check").style.display = "flex";
+    e.target.parentElement.querySelector(
+      ".todo__inputValue"
+    ).style.textDecoration = "line-through gray";
+    e.target.parentElement.querySelector(".todo__inputValue").style.color =
+      "gray";
     saveData();
-    // false;
-  } else if (e.target.classList.contains("cheked-icon")) {
-    e.target.parentElement.style.textDecoration = "none";
+  } else if (e.target.classList.contains("todo__check")) {
+    e.target.parentElement.querySelector(
+      ".todo__inputValue"
+    ).style.textDecoration = "none";
+    e.target.parentElement.querySelector(".todo__inputValue").style.color =
+      "#111";
     e.target.style.display = "none";
-    e.target.parentElement.querySelector(".check-icon").style.display =
+    e.target.parentElement.querySelector(".todo__square").style.display =
       "inline-block";
     saveData();
   }
