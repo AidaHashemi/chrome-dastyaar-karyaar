907aae08e6b0d00e9ab0b2ff33f38b8b8ef30851
Aida Hashemi <aidahashemiam@gmail.com>
2023-12-10 02:51:51 +0330

    Clock settings are done

--- assets/scripts/time.js
+++ assets/scripts/time.js
@@ -1,62 +1,25 @@
-// async function calculate() {
-//   const res = await fetch("https://api.dastyar.io/express/clock/current");
-//   const data = await res.json();
-//   const totalMilliseconds = data.current;
-//   var date = new Date(totalMilliseconds);
-//   var hour = date.getHours();
-//   var minutes = date.getMinutes();
-//   if (hour > 12) {
-//     const exectHour = hour - 12;
-//     hour = exectHour;
-//   }
-//   if (minutes > 30) {
-//     minutes -= 30;
-//   } else if (minutes <= 30) {
-//     minutes += 30;
-//   }
-//   if (minutes > 59) {
-//     hour += 1;
-//   }
-//   hour = hour < 10 ? "0" + hour : hour;
-//   minutes = minutes < 10 ? "0" + minutes : minutes;
+const time = document.getElementById("time");
+const digitalClockElement = document.createElement("span");
+digitalClockElement.setAttribute("class", "time__digital");
+time.prepend(digitalClockElement);
 
-//   curr_time = hour + ":" + minutes;
-//   console.log(curr_time);
-//   return curr_time;
-// }
-
-// calculate();
-
-// Calling showTime function at every second
-const digitalClock = document.querySelector(".time__digital");
-setInterval(showTime, 60000);
-
-// Defining showTime funcion
 async function showTime() {
-  // Getting current time and date
-  const res = await fetch("https://api.dastyar.io/express/clock/current");
-  const data = await res.json();
-  const totalMilliseconds = data.current;
-  var date = new Date(totalMilliseconds);
-  var hour = date.getHours();
-  var minutes = date.getMinutes();
-
-  // Setting time for 12 Hrs format
-  if (minutes < 30 && minutes > 0) {
-    minutes += 30;
+  try {
+    const res = await fetch("https://api.dastyar.io/express/clock/current");
+    const data = await res.json();
+    let totalMilliseconds = data.current;
+    let date = new Date(totalMilliseconds);
+    var hour = date.getHours();
+    var minutes = date.getMinutes();
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    hour = hour < 10 ? "0" + hour : hour;
+    var strTime = hour + ":" + minutes;
+    // String.prototype.toPersianDigits();
+    // const strong = `<strong>${strTime}</strong>`;
+    digitalClockElement.innerHTML = strTime.toPersianDigits();
+  } catch (err) {
+    console.log(err);
   }
-  if (hour > 12) {
-    hour -= 13;
-  } else if (hour == 0) {
-    hour = 12;
-  }
-  hour = hour < 10 ? "0" + hour : hour;
-  minutes = minutes < 10 ? "0" + minutes : minutes;
-  let currentTime = hour + ":" + minutes;
-
-  String.prototype.toPersianDigits();
-  var S = `<strong>${currentTime}</strong>`;
-  digitalClock.innerHTML = "" + S.toPersianDigits();
 }
 
 String.prototype.toPersianDigits = function () {
@@ -65,5 +28,5 @@ String.prototype.toPersianDigits = function () {
     return id[+w];
   });
 };
-
+setInterval(showTime, 60000);
 showTime();
